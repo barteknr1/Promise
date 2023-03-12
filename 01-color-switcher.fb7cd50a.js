a96f8fe6a063ea1960ec21587498c3c7ace97883
barteknr1 <114334919+barteknr1@users.noreply.github.com>
2023-03-12 08:13:09 +0000

    Deploying to gh-pages from @ barteknr1/goit-js-hw-09@b2c1469e3fc0f1222452c67b80f8af4b564ca52b 🚀

---
+++ 01-color-switcher.fb7cd50a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.body;document.querySelector(".color");let d=null;e.setAttribute("disabled","");t.addEventListener("click",(()=>{d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.removeAttribute("disabled"),t.setAttribute("disabled","")})),e.addEventListener("click",(()=>{clearInterval(d),t.removeAttribute("disabled"),e.setAttribute("disabled","")}));
+//# sourceMappingURL=01-color-switcher.fb7cd50a.js.map
